c507e6854e4324bb19ed9c4b00b5579681b07450
sayedraju1 <sayedraju21@gmail.com>
2022-06-09 18:28:12 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,44 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+// const withLess = require("next-with-less");
+
+// module.exports = withLess({
+//   lessLoaderOptions: {
+//     /* ... */
+//     lessOptions: {
+//       /* ... */
+//       modifyVars: {
+//         "primary-color": "#9FF",
+//         "border-radius-base": "2px",
+//         /* ... */
+//       },
+//     },
+//   },
+// });
+
+
+const withLess = require('next-with-less');
+const lessToJS = require('less-vars-to-js');
+
+const themeVariables = lessToJS(
+  fs.readFileSync(
+    path.resolve(__dirname, './public/styles/custom.less'),
+    'utf8'
+  )
+);
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables, // make your antd custom effective
+      localIdentName: '[path]___[local]___[hash:base64:5]',
+    },
+  },
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../public/styles/custom.less'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
